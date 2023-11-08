dc533495699cfc42df530dab003f3a174fde31be
Minh Duy <leduy06072003@gmail.com>
2023-11-08 11:39:40 +0700

    first commit

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const tabs = $$(".container-boarding");
-const panes = $$(".use-case");
+const panes = $$(".container-character");
 
 const tabActive = $(".container-boarding");
 const trigger = $(".trigger");
-
 requestIdleCallback(function () {
   trigger.style.left = tabActive.offsetLeft + "px";
   trigger.style.width = tabActive.offsetWidth + "px";
   trigger.style.height = tabActive.offsetHeight + "px";
+
 });
 
 tabs.forEach((tab, index) => {
@@ -18,7 +18,7 @@ tabs.forEach((tab, index) => {
 
   tab.onclick = function () {
     $(".container-boarding.active").classList.remove("active");
-    $(".use-case.new").classList.remove("new");
+    $(".container-character.new").classList.remove("new");
 
     trigger.style.left = this.offsetLeft + "px";
     trigger.style.width = this.offsetWidth + "px";
